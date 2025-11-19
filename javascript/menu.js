7fac679afa01add60d09ca275af04093c5dc05f1
snowfluk3 <lukeumali015@gmail.com>
2025-11-19 11:59:32 -0800

    Test v1.0.2 for all pages

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,15 +1,40 @@
-var dropdown = document.getElementsByClassName("dropdown-button");
-var i;
+const drinkButton = document.getElementById("drink-button")
+const foodButton = document.getElementById("food-button")
+const seasonalButton = document.getElementById("seasonal-button")
 
 
-for (i = 0; i < dropdown.length; i++) {
-    dropdown[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var dropdownContent = this.nextElementSibling;
-    if (dropdownContent.style.display === "block") {
-        dropdownContent.style.display = "none";
-    } else {
-        dropdownContent.style.display = "block";
-    }
-    });
-}
+drinkButton.addEventListener('click', function() {
+    const addContent = document.querySelector("#drinks")
+    addContent.classList.remove("inactive")
+    addContent.classList.add("active")
+
+    const removeContent = document.querySelectorAll("#foods, #seasonal")
+    removeContent.forEach(element => {
+        element.classList.remove("active")
+        element.classList.add("inactive") 
+    })
+})
+
+foodButton.addEventListener('click', function() {
+    const addContent = document.querySelector("#foods")
+    addContent.classList.remove("inactive")
+    addContent.classList.add("active")
+
+    const removeContent = document.querySelectorAll("#drinks, #seasonal")
+    removeContent.forEach(element => {
+        element.classList.remove("active")
+        element.classList.add("inactive") 
+    })
+})
+
+seasonalButton.addEventListener('click', function() {
+    const addContent = document.querySelector("#seasonal")
+    addContent.classList.remove("inactive")
+    addContent.classList.add("active")
+
+    const removeContent = document.querySelectorAll("#drinks, #foods")
+    removeContent.forEach(element => {
+        element.classList.remove("active")
+        element.classList.add("inactive") 
+    })
+})
